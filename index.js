5faa1737f936703b9d8a23fb6121d944501182fe
Josuepf <josuepontes000@gmail.com>
2025-07-08 20:59:19 -0300

    versão inicial do backend

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const FILE = 'data.json';
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/tarefas', (req, res) => {
+    fs.readFile(FILE, 'utf8', (err, data) => {
+      if (err) return res.status(500).json({ erro: 'Erro ao ler arquivo' });
+      res.json(JSON.parse(data));
+    });
+  });  
+
+  app.post('/api/tarefas', (req, res) => {
+    fs.writeFile(FILE, JSON.stringify(req.body, null, 2), err => {
+      if (err) return res.status(500).json({ erro: 'Erro ao salvar arquivo' });
+      res.json({ sucesso: true });
+    });
+  });
+  
+  
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
