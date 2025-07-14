5e80b4e7317c6f1ca7e285991af530065db759c0
Josuepf <josuepontes000@gmail.com>
2025-07-13 21:20:25 -0300

    ativando CORS para frontend

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
-const DATA_FILE = path.join(__dirname, 'data.json');
-
-app.use(cors());
+app.use(cors());  // <- libera CORS
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
